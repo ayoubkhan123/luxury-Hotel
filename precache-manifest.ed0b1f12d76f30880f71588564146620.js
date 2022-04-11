70c6ec2fe68de990986a66bdea2e2a2b45a0bedb
AYYub khan <ayubkhan666304@gmail.com>
2022-04-11 20:51:01 +0500

    Updates

---
+++ precache-manifest.ed0b1f12d76f30880f71588564146620.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "16a6439bdb06c35d87cb",
+    "url": "/luxuryhotel/static/css/main.cdccf2f4.chunk.css"
+  },
+  {
+    "revision": "16a6439bdb06c35d87cb",
+    "url": "/luxuryhotel/static/js/main.16a6439b.chunk.js"
+  },
+  {
+    "revision": "845e2c4320ee3d7edf47",
+    "url": "/luxuryhotel/static/js/1.845e2c43.chunk.js"
+  },
+  {
+    "revision": "2dd16b889ed212c4ff16",
+    "url": "/luxuryhotel/static/css/2.39483483.chunk.css"
+  },
+  {
+    "revision": "2dd16b889ed212c4ff16",
+    "url": "/luxuryhotel/static/js/2.2dd16b88.chunk.js"
+  },
+  {
+    "revision": "ba368d440084cba9517f",
+    "url": "/luxuryhotel/static/js/runtime~main.ba368d44.js"
+  },
+  {
+    "revision": "06e4688466115be0ab4031fe5163f2f0",
+    "url": "/luxuryhotel/static/media/visualsofdana-T5pL6ciEn-I-unsplash.06e46884.jpg"
+  },
+  {
+    "revision": "f040be0fa7b0ecb3615e882616e37496",
+    "url": "/luxuryhotel/static/media/hotel-room-13225019.f040be0f.jpg"
+  },
+  {
+    "revision": "02291ed36655250d606613774925d4c5",
+    "url": "/luxuryhotel/static/media/depositphotos_3683191-stock-photo-king-size-bed-and-bathroom.02291ed3.jpg"
+  },
+  {
+    "revision": "3fdf1f7e1f9ab76b42a3ae6337c779fb",
+    "url": "/luxuryhotel/static/media/pexels-photo-164595.3fdf1f7e.jpeg"
+  },
+  {
+    "revision": "963bbe79919c116dadfbb3c5fecad487",
+    "url": "/luxuryhotel/static/media/mara-conan-design-O_6dZUyxezo-unsplash.963bbe79.jpg"
+  },
+  {
+    "revision": "0b43ab5ff007e4395e84b01bbda3bd1c",
+    "url": "/luxuryhotel/static/media/manuel-moreno-DGa0LQ0yDPc-unsplash.0b43ab5f.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/luxuryhotel/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/luxuryhotel/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/luxuryhotel/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/luxuryhotel/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "693199e9eced993f72248c3b2a57cd6c",
+    "url": "/luxuryhotel/static/media/jarvis-hotels-logo-svg-vector.693199e9.svg"
+  },
+  {
+    "revision": "3326bd905ef99d2975b2a18567d3ac7e",
+    "url": "/luxuryhotel/static/media/marten-bjork-n_IKQDCyrG0-unsplash.3326bd90.jpg"
+  },
+  {
+    "revision": "24381ee33b95c578c6bed2baf57310c1",
+    "url": "/luxuryhotel/static/media/fernando-alvarez-rodriguez-M7GddPqJowg-unsplash.24381ee3.jpg"
+  },
+  {
+    "revision": "af8020097021a05f3da5f78b00736273",
+    "url": "/luxuryhotel/index.html"
+  }
+];
